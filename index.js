630f68fc3d64fe94834b3ad191c34d6517d066e7
amitreal11 <128671695+amitreal11@users.noreply.github.com>
2023-08-02 23:33:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send({status: "true", message: "connected to server"})
 })
 
-app.listen(8080, ()=>{
-    console.log(">>>>>server running on port 8080");
+app.listen(5050, ()=>{
+    console.log(">>>>>server running on port 5050");
 })
